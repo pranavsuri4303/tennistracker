03433d63db4b0c645f70a3cfe3207e71917def68
Pranav Suri <pranav.suri@uwaterloo.ca>
2022-12-29 03:29:00 -0500

    Update swiftgen and stuff

---
+++ apple/Tennis/Icons-Constants.swift
@@ -0,0 +1,103 @@
+// swiftlint:disable all
+// Generated using SwiftGen — https://github.com/SwiftGen/SwiftGen
+
+#if os(macOS)
+    import AppKit
+#elseif os(iOS)
+    import UIKit
+#elseif os(tvOS) || os(watchOS)
+    import UIKit
+#endif
+#if canImport(SwiftUI)
+    import SwiftUI
+#endif
+
+// Deprecated typealiases
+
+// swiftlint:disable superfluous_disable_command file_length implicit_return
+
+// MARK: - Asset Catalogs
+
+// swiftlint:disable identifier_name line_length nesting type_body_length type_name
+internal enum Icons {
+    internal static let ball = SymbolAsset(name: "ball")
+    internal static let court2 = SymbolAsset(name: "court2")
+    internal static let racketBall = SymbolAsset(name: "racket.ball")
+}
+
+// swiftlint:enable identifier_name line_length nesting type_body_length type_name
+
+// MARK: - Implementation Details
+
+internal struct SymbolAsset {
+    internal fileprivate(set) var name: String
+
+    #if os(iOS) || os(tvOS) || os(watchOS)
+        @available(iOS 13.0, tvOS 13.0, watchOS 6.0, *)
+        internal typealias Configuration = UIImage.SymbolConfiguration
+        internal typealias Image = UIImage
+
+        @available(iOS 12.0, tvOS 12.0, watchOS 5.0, *)
+        internal var image: Image {
+            let bundle = BundleToken.bundle
+            #if os(iOS) || os(tvOS)
+                let image = Image(named: name, in: bundle, compatibleWith: nil)
+            #elseif os(watchOS)
+                let image = Image(named: name)
+            #endif
+            guard let result = image else {
+                fatalError("Unable to load symbol asset named \(name).")
+            }
+            return result
+        }
+
+        @available(iOS 13.0, tvOS 13.0, watchOS 6.0, *)
+        internal func image(with configuration: Configuration) -> Image {
+            let bundle = BundleToken.bundle
+            guard let result = Image(named: name, in: bundle, with: configuration) else {
+                fatalError("Unable to load symbol asset named \(name).")
+            }
+            return result
+        }
+    #endif
+
+    #if canImport(SwiftUI)
+        @available(iOS 13.0, tvOS 13.0, watchOS 6.0, macOS 10.15, *)
+        internal var swiftUIImage: SwiftUI.Image {
+            SwiftUI.Image(asset: self)
+        }
+    #endif
+}
+
+#if canImport(SwiftUI)
+    @available(iOS 13.0, tvOS 13.0, watchOS 6.0, macOS 10.15, *)
+    internal extension SwiftUI.Image {
+        init(asset: SymbolAsset) {
+            let bundle = BundleToken.bundle
+            self.init(asset.name, bundle: bundle)
+        }
+
+        init(asset: SymbolAsset, label: Text) {
+            let bundle = BundleToken.bundle
+            self.init(asset.name, bundle: bundle, label: label)
+        }
+
+        init(decorative asset: SymbolAsset) {
+            let bundle = BundleToken.bundle
+            self.init(decorative: asset.name, bundle: bundle)
+        }
+    }
+#endif
+
+// swiftlint:disable convenience_type
+private final class BundleToken {
+    static let bundle: Bundle = {
+        #if SWIFT_PACKAGE
+            return Bundle.module
+        #else
+            return Bundle(for: BundleToken.self)
+        #endif
+    }()
+}
+
+// swiftlint:enable convenience_type
