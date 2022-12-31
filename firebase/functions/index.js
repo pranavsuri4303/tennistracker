d548255d8c93cf7c573dda027b1b27bd21d1242f
Pranav Suri <pranav.suri@uwaterloo.ca>
2022-12-30 21:31:37 -0500

    Cloud functions

---
+++ firebase/functions/index.js
@@ -0,0 +1,56 @@
+const functions = require("firebase-functions");
+
+const { initializeApp } = require('firebase-admin/app');
+const { getFirestore, Timestamp, FieldValue } = require('firebase-admin/firestore');
+initializeApp();
+
+const db = getFirestore();
+
+exports.createUserDocs = functions.auth.user().beforeCreate((data,context) => {
+    const uid = data.uid;
+    const email = data.email;
+    const userDocRef = db.collection('users').doc(uid);
+    const exDocRef = db.collection('users').doc(uid).collection('extensions');
+    const friendshipsDocRef = exDocRef.doc('friendships');
+    const stringsDocRef = exDocRef.doc('stringEntries');
+    const matchesDocRef = exDocRef.doc('matches');
+
+    const userDocData = {
+        emailAddress: email,
+        personId: uid,
+        updated: context.timestamp
+    };
+
+    const friendshipsDocData = {
+        count: 0,
+        friendsCount: 0,
+        sentRequestsCount: 0,
+        pendingRequestsCount: 0,
+        personId: uid,
+        updated: context.timestamp,
+        friendships: []
+    };
+
+    const matchesDocData = {
+        personId: uid,
+        matches: [],
+        updated: context.timestamp
+    };
+
+    const stringsDocData = {
+        count: 0,
+        updated: context.timestamp,
+        stringEntries: [],
+        personID: uid
+    };
+
+    try {
+        userDocRef.set(userDocData);
+        friendshipsDocRef.set(friendshipsDocData);
+        stringsDocRef.set(stringsDocData);
+        matchesDocRef.set(matchesDocData);
+        return;
+    } catch (e) {
+        return functions.auth.HttpsError('invalid-argument')
+    }
+  });
