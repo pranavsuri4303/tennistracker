6bbe65f48a4048cf3d546078988f3815d37da005
Pranav Suri <pranav.suri@uwaterloo.ca>
2023-03-07 20:35:50 -0500

    - change colors - add matchup format view and vm

--- apple/Tennis/View/Match/MatchUpFormatView.swift
+++ apple/Tennis/View/Match/MatchUpFormatView.swift
@@ -8,8 +8,21 @@
 import SwiftUI
 
 struct MatchUpFormatView: View {
+    @ObservedObject var vm = MatchUpVM()
+    @State var buttonState = XelaButtonState.Default
     @State var tempText1 = ""
     @State var selectedId: Int = 0
+    @State var selectedId1: Int = 0
+    @State var selectedId2: Int = 0
+    @State var selectedId3: Int = 5
+    @State var selectedId4: Bool = false
+    @State var selectedId5: Bool = false
+    @State var selectedId6: Int = 0
+    @State var selectedId7: Int = 0
+    @State var selectedId8: Int = 0
+    @State var selectedId9: Int = 0
+    @State var selectedId10: Int = 0
+    @State var selectedId11: Bool = false
     @State var tempText2 = ""
     @State var tempText3 = ""
     @State var tempText4 = ""
@@ -17,19 +30,157 @@ struct MatchUpFormatView: View {
     @State var tempText6 = ""
     var body: some View {
         VStack(spacing: 12) {
-            HStack(alignment: .center, spacing: 5){
-                VStack{
-                    Text("Best of/Exactly")
-                        .xelaBodyBold()
-                    XelaSegmentedControl(items: [XelaSegmentedControlItem(id: 0,
-                                                                          label: "Best of"),
-                                                 XelaSegmentedControlItem(id: 1,
-                                                                          label: "Exactly")], selectedId: $selectedId)
+            HStack {
+                Text("Match Format")
+                    .xelaHeadline()
+                Spacer()
+            }
+            List{
+                Section("Match Format") {
+//                    Picker(selection: $vm.ma) {
+//                        <#code#>
+//                    } label: {
+//                        <#code#>
+//                    }
+                    
+                    Picker(selection: $vm.matchUpFormat.bestOf) {
+//                        ForEach(NoOfSets.allCases, id: self) { <#Data.Element#> in
+//                            <#code#>
+//                        }
+                        ForEach(NoOfSets.allCases) { noOfSets in
+                            Text("\(noOfSets.value)")
+                                .tag(noOfSets)
+                        }
+                    } label: {
+                        Text("No. of sets")
+                            .xelaBody()
+                    }
+                    Picker("test", selection: $vm.matchUpFormat.bestOf) {
+                        ForEach(NoOfSets.allCases) { noOfSets in
+                            Text("\(noOfSets.value)")
+//                                .tag(noOfSets)
+                        }
+                    }
+                    Picker(selection: $selectedId2) {
+                        Text("Advantage").tag(0)
+                        Text("No Ad").tag(1)
+                    } label: {
+                        Text("Ad / No Ad")
+                            .xelaBody()
+                    }
+                    Picker(selection: $selectedId3) {
+                        Text("1").tag(0)
+                        Text("2").tag(1)
+                        Text("3").tag(2)
+                        Text("4").tag(3)
+                        Text("5").tag(4)
+                        Text("6").tag(5)
+                        Text("7").tag(6)
+                        Text("8").tag(7)
+                        Text("9").tag(8)
+                    } label: {
+                        Text("No. of games")
+                            .xelaBody()
+                    }
+                    Toggle(isOn: $selectedId4) {
+                        Text("Tiebreak")
+                            .xelaBody()
+                    }
+                    Toggle(isOn: $selectedId5) {
+                        Text("Final Set")
+                            .xelaBody()
+                    }
+                }
+                if selectedId4{
+                    Section("Tiebreak Format") {
+                        Picker(selection: $selectedId6) {
+                            Text("5").tag(0)
+                            Text("7").tag(1)
+                            Text("9").tag(2)
+                            Text("10").tag(3)
+                            Text("12").tag(4)
+                        } label: {
+                            Text("Tiebreak to")
+                                .xelaBody()
+                        }
+                        Picker(selection: $selectedId7) {
+                            Text("\(selectedId3)-\(selectedId3)").tag(0)
+                            Text("\(selectedId3 + 1)-\(selectedId3 + 1)").tag(1)
+                        } label: {
+                            Text("Tiebreak at")
+                                .xelaBody()
+                        }
+                        Picker(selection: $selectedId8) {
+                            Text("1").tag(0)
+                            Text("2").tag(1)
+                        } label: {
+                            Text("Win by")
+                                .xelaBody()
+                        }
+                        
+                    }
+                }
+                if selectedId5 {
+                    Section("Final set format") {
+                        Picker(selection: $selectedId9) {
+                            Text("Ad").tag(0)
+                            Text("No Ad").tag(1)
+                        } label: {
+                            Text("Ad / No Ad")
+                                .xelaBody()
+                        }
+                        Picker(selection: $selectedId10) {
+                            Text("1").tag(0)
+                            Text("2").tag(1)
+                            Text("3").tag(2)
+                            Text("4").tag(3)
+                            Text("5").tag(4)
+                            Text("6").tag(5)
+                            Text("7").tag(6)
+                            Text("8").tag(7)
+                            Text("9").tag(8)
+                        } label: {
+                            Text("No. of games")
+                                .xelaBody()
+                        }
+                        Toggle(isOn: $selectedId11) {
+                            Text("Tiebreak")
+                                .xelaBody()
+                        }
+                        
+                    }
+                }
+                if selectedId11 {
+                    Section("Final set format") {
+                        Text("Final Set Format")
+                    }
                 }
             }
-            XelaTextField(placeholder: "No. of Set", value: $tempText2, state: .Default, isDataPicker: true)
+            .listStyle(.insetGrouped)
+            .background(Color.clear)
+            .modifier(ListBackgroundModifier())
+            
+            NavigationLink {
+                Text("Player Details")
+            } label: {
+                XelaButton(text: "Test", state: $buttonState)
+            }
+
         }
-        .padding(.horizontal, 24)
+        //        .padding(.horizontal, 24)
+        .background(Color(asset: Colors.background).ignoresSafeArea(.all, edges: .all))
+        
     }
 }
-
+struct ListBackgroundModifier: ViewModifier {
+    
+    @ViewBuilder
+    func body(content: Content) -> some View {
+        if #available(iOS 16.0, *) {
+            content
+                .scrollContentBackground(.hidden)
+        } else {
+            content
+        }
+    }
+}
--- apple/Tennis/View/Match/MatchUpVM.swift
+++ apple/Tennis/View/Match/MatchUpVM.swift
@@ -6,7 +6,196 @@
 //
 
 import Foundation
-class MatchUpVM: ObservableObject {    
+class MatchUpVM: ObservableObject {
+    @Published var matchUpFormat = MatchUpFormatModel(bestOf: NoOfSets.three, setFormat: SetFormatModel(noAd: false, tiebreakAt: 0, setTo: 1, tiebreakFormat: TiebreakFormatModel(noAd: false, tiebreakTo: 0)))
+}
+
+struct MatchUpFormatModel: Codable, Equatable {
+    internal init(bestOf: NoOfSets? = nil, setFormat: SetFormatModel? = nil, finalSetFormat: SetFormatModel? = nil) {
+        self.bestOf = bestOf
+        self.setFormat = setFormat
+        self.finalSetFormat = finalSetFormat
+    }
+    
+    var bestOf: NoOfSets?
+    var setFormat: SetFormatModel?
+    var finalSetFormat: SetFormatModel?
+    
+    static func == (lhs: MatchUpFormatModel, rhs: MatchUpFormatModel) -> Bool {
+        return lhs.bestOf == rhs.bestOf &&
+        lhs.setFormat == rhs.setFormat &&
+        lhs.finalSetFormat == rhs.finalSetFormat
+    }
+    
+    enum CodingKeys: CodingKey {
+        case bestOf
+        case setFormat
+        case finalSetFormat
+    }
+    
+    init(from decoder: Decoder) throws {
+        let container: KeyedDecodingContainer<MatchUpFormatModel.CodingKeys> = try decoder.container(keyedBy: MatchUpFormatModel.CodingKeys.self)
+        
+        self.bestOf = try container.decodeIfPresent(NoOfSets.self, forKey: MatchUpFormatModel.CodingKeys.bestOf)
+        self.setFormat = try container.decodeIfPresent(SetFormatModel.self, forKey: MatchUpFormatModel.CodingKeys.setFormat)
+        self.finalSetFormat = try container.decodeIfPresent(SetFormatModel.self, forKey: MatchUpFormatModel.CodingKeys.finalSetFormat)
+        
+    }
+    
+    func encode(to encoder: Encoder) throws {
+        var container: KeyedEncodingContainer<MatchUpFormatModel.CodingKeys> = encoder.container(keyedBy: MatchUpFormatModel.CodingKeys.self)
+        
+        try container.encodeIfPresent(self.bestOf, forKey: MatchUpFormatModel.CodingKeys.bestOf)
+        try container.encodeIfPresent(self.setFormat, forKey: MatchUpFormatModel.CodingKeys.setFormat)
+        try container.encodeIfPresent(self.finalSetFormat, forKey: MatchUpFormatModel.CodingKeys.finalSetFormat)
+    }
+}
+
+struct SetFormatModel: Codable, Equatable {
+    internal init(noAd: Bool? = nil, tiebreakAt: Int? = nil, setTo: Int? = nil, tiebreakFormat: TiebreakFormatModel? = nil) {
+        self.noAd = noAd
+        self.tiebreakAt = tiebreakAt
+        self.setTo = setTo
+        self.tiebreakFormat = tiebreakFormat
+    }
+    
+    var noAd: Bool?
+    var tiebreakAt: Int?
+    var setTo: Int?
+    var tiebreakFormat: TiebreakFormatModel?
+    
+    static func == (lhs: SetFormatModel, rhs: SetFormatModel) -> Bool {
+        return lhs.noAd == rhs.noAd &&
+        lhs.tiebreakAt == rhs.tiebreakAt &&
+        lhs.setTo == rhs.setTo &&
+        lhs.tiebreakFormat == rhs.tiebreakFormat
+    }
+    
+    enum CodingKeys: CodingKey {
+        case noAd
+        case tiebreakAt
+        case setTo
+        case tiebreakFormat
+    }
+    
+    init(from decoder: Decoder) throws {
+        let container: KeyedDecodingContainer<SetFormatModel.CodingKeys> = try decoder.container(keyedBy: SetFormatModel.CodingKeys.self)
+        
+        self.noAd = try container.decodeIfPresent(Bool.self, forKey: SetFormatModel.CodingKeys.noAd)
+        self.tiebreakAt = try container.decodeIfPresent(Int.self, forKey: SetFormatModel.CodingKeys.tiebreakAt)
+        self.setTo = try container.decodeIfPresent(Int.self, forKey: SetFormatModel.CodingKeys.setTo)
+        self.tiebreakFormat = try container.decodeIfPresent(TiebreakFormatModel.self, forKey: SetFormatModel.CodingKeys.tiebreakFormat)
+        
+    }
+    
+    func encode(to encoder: Encoder) throws {
+        var container: KeyedEncodingContainer<SetFormatModel.CodingKeys> = encoder.container(keyedBy: SetFormatModel.CodingKeys.self)
+        
+        try container.encodeIfPresent(self.noAd, forKey: SetFormatModel.CodingKeys.noAd)
+        try container.encodeIfPresent(self.tiebreakAt, forKey: SetFormatModel.CodingKeys.tiebreakAt)
+        try container.encodeIfPresent(self.setTo, forKey: SetFormatModel.CodingKeys.setTo)
+        try container.encodeIfPresent(self.tiebreakFormat, forKey: SetFormatModel.CodingKeys.tiebreakFormat)
+    }
+}
+
+struct TiebreakFormatModel: Codable, Equatable {
+    internal init(noAd: Bool? = nil, tiebreakTo: Int? = nil) {
+        self.noAd = noAd
+        self.tiebreakTo = tiebreakTo
+    }
+    
+    var noAd: Bool?
+    var tiebreakTo: Int?
+    
+    static func == (lhs: TiebreakFormatModel, rhs: TiebreakFormatModel) -> Bool {
+        return lhs.noAd == rhs.noAd &&
+        lhs.tiebreakTo == rhs.tiebreakTo
+    }
+    
+    enum CodingKeys: CodingKey {
+        case noAd
+        case tiebreakTo
+    }
+    
+    init(from decoder: Decoder) throws {
+        let container: KeyedDecodingContainer<TiebreakFormatModel.CodingKeys> = try decoder.container(keyedBy: TiebreakFormatModel.CodingKeys.self)
+        
+        self.noAd = try container.decodeIfPresent(Bool.self, forKey: TiebreakFormatModel.CodingKeys.noAd)
+        self.tiebreakTo = try container.decodeIfPresent(Int.self, forKey: TiebreakFormatModel.CodingKeys.tiebreakTo)
+        
+    }
+    
+    func encode(to encoder: Encoder) throws {
+        var container: KeyedEncodingContainer<TiebreakFormatModel.CodingKeys> = encoder.container(keyedBy: TiebreakFormatModel.CodingKeys.self)
+        
+        try container.encodeIfPresent(self.noAd, forKey: TiebreakFormatModel.CodingKeys.noAd)
+        try container.encodeIfPresent(self.tiebreakTo, forKey: TiebreakFormatModel.CodingKeys.tiebreakTo)
+    }
 }
 
 
+enum NoOfSets: String, CaseIterable, Identifiable {
+    case one
+    case three
+    case five
+    
+    var value: Int {
+        switch self {
+        case .one: return 1
+        case .three: return 3
+        case .five: return 5
+        }
+    }
+
+    var code: String {
+        switch self {
+        case .one: return "1"
+        case .three: return "3"
+        case .five: return "5"
+        }
+    }
+
+    var extendedCode: String {
+        switch self {
+        case .one: return "1 Set"
+        case .three: return "3 Sets"
+        case .five: return "5 Sets"
+        }
+    }
+
+    var description: String {
+        switch self {
+        case .one: return "Best of 1 Set"
+        case .three: return "Best of 3 Sets"
+        case .five: return "Best of 5 Sets"
+        }
+    }
+
+    var id: NoOfSets { self }
+
+    static func getNoOfSets(noOfSets: String) -> NoOfSets {
+        switch noOfSets {
+        case "1":
+            return .one
+        case "3":
+            return .three
+        case "5":
+            return .five
+        default:
+            return .three
+        }
+    }
+}
+
+extension NoOfSets: Codable {
+    init(from decoder: Decoder) throws {
+        let container = try decoder.singleValueContainer()
+        let rawValue = try container.decode(String.self)
+        self = NoOfSets.getNoOfSets(noOfSets: rawValue)
+    }
+
+    func encode(to encoder: Encoder) throws {
+        var container = encoder.singleValueContainer()
+        try container.encode(code)
+    }
+}
